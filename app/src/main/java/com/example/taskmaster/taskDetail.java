826e844edfa4cb4d1768f2ea95bc778111a52f58
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-31 13:28:04 +0300

    add back button to action bar

--- app/src/main/java/com/example/taskmaster/taskDetail.java
+++ app/src/main/java/com/example/taskmaster/taskDetail.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class taskDetail extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
     @Override
         protected void onStart() {
@@ -21,5 +26,11 @@ public class taskDetail extends AppCompatActivity {
             TextView title = findViewById(R.id.titleTask);
             title.setText(name);
         }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+    }
 
 }
