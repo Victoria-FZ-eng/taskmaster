64ea6fffdd502c43c909c09d4fc9f0792d9e03f4
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-01 18:12:42 +0300

    add dependencies and add entity

--- app/src/main/java/com/example/taskmaster/Task.java
+++ app/src/main/java/com/example/taskmaster/Task.java
@@ -1,16 +1,22 @@
 package com.example.taskmaster;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity
 public class Task {
 
-    public String title;
-    public String body;
-    // TODO: state should be one of “new”, “assigned”, “in progress”, or “complete”.
-    public String state;
+    @PrimaryKey
+    public int idTask;
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int id;
+    @ColumnInfo(name="title")
+    public String title;
+    @ColumnInfo(name="body")
+    public String body;
+    @ColumnInfo(name="state")
+    public String state;
+    
 
     public Task(String title, String body, String state) {
         this.title = title;
