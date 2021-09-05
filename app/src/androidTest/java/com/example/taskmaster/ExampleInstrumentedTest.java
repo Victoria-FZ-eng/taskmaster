00ea551596c9d8638155e5d4d2856a131e46aee6
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-05 12:06:21 +0300

    adding some UI tests

--- app/src/androidTest/java/com/example/taskmaster/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/taskmaster/ExampleInstrumentedTest.java
@@ -2,12 +2,24 @@ package com.example.taskmaster;
 
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static java.util.EnumSet.allOf;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +28,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
@@ -23,4 +36,46 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.taskmaster", appContext.getPackageName());
     }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+
+    @Test
+    public void checkAllDisplayed() {
+        onView(withId(R.id.tskUser)).check((matches(isDisplayed())));
+        onView(withId(R.id.btn1)).check((matches(isDisplayed())));
+        onView(withId(R.id.textView8)).check((matches(isDisplayed())));
+        onView(withId(R.id.recViewTask)).check((matches(isDisplayed())));
+        onView(withId(R.id.settings)).check((matches(isDisplayed())));
+    }
+    //withText
+    @Test
+    public void settingsButton() {
+        onView(withId(R.id.settings)).check((matches(isDisplayed()))).perform(click());
+    }
+    @Test
+    public void addTaskButton() {
+        onView(withId(R.id.btn1)).check((matches(isDisplayed()))).perform(click());
+    }
+    @Test
+    public void fragTitleButton() {
+        onData(withId(R.id.recViewTask)).perform(click());
+    }
+
+
+    @Rule
+    public ActivityScenarioRule<addTask> activityAddTaskRule =
+            new ActivityScenarioRule<>(addTask.class);
+
+    @Test
+    public void testAddingTask(){
+        onView(withId(R.id.btn3)).check((matches(isDisplayed()))).perform(click());
+    }
+
+
+
+
 }
+
